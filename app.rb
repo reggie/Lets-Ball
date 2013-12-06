00dea99b3f01209e006ca7bf18982b51388f992c
Reggiemon <reggiemontilus@gmail.com>
2013-12-06 01:03:47 -0500

    added reject

--- app.rb
+++ app.rb
@@ -2,11 +2,16 @@ require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
 
-get '/sms' do
+post '/sms' do
 	twiml = Twilio::TwiML::Response.new do |r|
 		r.Message "Response given."
 	end
 	twiml.text
+end
+
+post '/call' do
+	twiml = Twilio::TwiML::Response.new do |r|
+		r.Reject
 end
 
 get '/' do
