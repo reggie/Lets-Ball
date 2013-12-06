1c7a1aae1319a76e12c44cb13fce535f60c74f78
Reggiemon <reggiemontilus@gmail.com>
2013-12-05 23:52:44 -0500

    intial commit

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'twilio-ruby'
+require 'sinatra'
+
+get '/sms-quickstart' do
+	twiml = Twilio::TwiML::Response.new do |r|
+		r.Message "Response given."
+	end
+	twiml.text
+end
+
+get '/' do
+	"Twilio Time"
+end
+
+
