88dc28ee9fb371fb6a0e9f32628016ad14e95206
Reggiemon <reggiemontilus@gmail.com>
2013-12-26 21:36:23 -0500

    added help menu

--- app.rb
+++ app.rb
@@ -9,6 +9,18 @@ post '/sms' do
 		twiml = Twilio::TwiML::Response.new do |r|
 			r.Message "#{messageTokens[1]} was added."
 		end
+	when "-r"
+		twiml = Twilio::TwiML::Response.new do |r|
+			r.Message "#{messageTokens[1]} was removed."
+		end
+	when "-b"
+		twiml = Twilio::TwiML::Response.new do |r|
+			r.Message "Ball request: #{messageTokens[1]} at #{messageTokens[2]} - created."
+		end
+	when "-h"
+		twiml = Twilio::TwiML::Response.new do |r|
+			r.Message "Valid Inputs:\n\tAdd\n\t-a <name>\n\tRemove\n\t-r <name>\n\tBall\n\t-b <location> <time>"
+		end
 	else
 		twiml = Twilio::TwiML::Response.new do |r|
 			r.Message "Invalid input sent. Text -h for help."
