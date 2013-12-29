e2c7104afd5f77abcc660d98a436dabe992b32e3
Reggiemon <reggiemontilus@gmail.com>
2013-12-29 14:25:42 -0500

    Trying t DRY out the code

--- app.rb
+++ app.rb
@@ -4,29 +4,31 @@ require 'sinatra'
 
 post '/sms' do
 	messageTokens = params[:Body].split
-	twiml = Twilio::TwiML::Response.new do |r|
-		case messageTokens[0]
-		when "-a"
-			if messageTokens[1] == nil
-				r.Message "No name was given."
-			else
-				r.Message "#{messageTokens[1]} was added."
-			end
-		when "-r"
-			r.Message "#{messageTokens[1]} was removed."
-		when "-b"
-			if messageTokens[2] == nil 
-				r.Message "The ball request was not formatted properly."
-			else
-				r.Message "Ball request: #{messageTokens[1]} at #{messageTokens[2]} - created."
-			end
-		when "-h"
-			r.Message "Valid Inputs:\n\tAdd Baller\n\t-a <name>\n\tRemove Baller\n\t-r <name>\n\tBall Request\n\t-b <location> <time>"
+	case messageTokens[0]
+	message = ""
+	when "-a"
+		if messageTokens[1] == nil
+			message = "No name was given."
 		else
-			r.Message "Invalid input sent. Text -h for help."
+			message = "#{messageTokens[1]} was added."
 		end
-		twiml.text
+	when "-r"
+		message = "#{messageTokens[1]} was removed."
+	when "-b"
+		if messageTokens[2] == nil 
+			message = "The ball request was not formatted properly."
+		else
+			message = "Ball request: #{messageTokens[1]} at #{messageTokens[2]} - created."
+		end
+	when "-h"
+		message = "Valid Inputs:\n\tAdd Baller\n\t-a <name>\n\tRemove Baller\n\t-r <name>\n\tBall Request\n\t-b <location> <time>"
+	else
+		message =  "Invalid input sent. Text -h for help."
 	end
+	twiml = Twilio::TwiML::Response.new do |r|
+		r.Message message
+	end
+	twiml.text
 end
 
 post '/call' do
